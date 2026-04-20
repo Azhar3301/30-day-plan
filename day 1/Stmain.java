c4933ec2ffb8245f3c50954ddb3fc438ad5f0949
Azhar3301 <it24031@mbstu.ac.bd>
2026-04-20 08:04:43 +0600

    Add files via upload

---
+++ day 1/Stmain.java
@@ -0,0 +1,10 @@
+public class Stmain{
+    static void main(String[] args){
+        Student s=new Student();
+        s.name="Azhar";
+        s.Roll= "IT-24031";
+        s.Number=78;
+        s.Show();
+       
+    }
+}
---
+++ day 1/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name;
+    String Roll;
+    int Number;
+    void Show() {
+        System.out.println("Student's name: "+name);
+        System.out.println("Student's roll: "+Roll);
+        System.out.println("Student's number: "+Number);
+    }
+}
