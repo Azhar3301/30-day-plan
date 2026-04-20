aea61207402b1c55d2c2545960379402df748c05
Azhar3301 <it24031@mbstu.ac.bd>
2026-04-20 07:54:18 +0600

    Add files via upload

---
+++ day 1/Cmain.java
@@ -0,0 +1,9 @@
+public class Cmain {
+     public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="red";
+  myCar.model="BMW";
+  myCar.year=2026;
+  myCar.displayInfo();
+    }
+}
