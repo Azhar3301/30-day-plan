1080bffcaad2e7ed054aefd2fbcb3e4d322649e6
Azhar3301 <it24031@mbstu.ac.bd>
2026-04-22 09:23:19 +0600

    Update Example2.java

--- Day 2/Example2.java
+++ Day 2/Example2.java
@@ -1,35 +1,30 @@
 class Student {
     private String studentId;
     private char grade;
-
     public String getStudentId() {
         return studentId;
     }
-
     public void setStudentId(String studentId) {
         this.studentId = studentId;
     }
-
     public char getGrade() {
         return grade;
     }
-
     public void setGrade(char grade) {
-        if (grade == 'A' || grade == 'B' || grade == 'C') {
+        if (grade=='A'||grade=='B'||grade=='C') {
             this.grade = grade;
         } else {
             System.out.println("Invalid Grade");
         }
     }
 }
-
 public class SchoolSystem {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setStudentId("ICT-101");
-        s1.setGrade('A');
+        s1.setStudentId("It24031");
+        s1.setGrade('B');
 
-        System.out.println("Student: " + s1.getStudentId());
+        System.out.println("Student: "+s1.getStudentId());
         System.out.println("Grade: " + s1.getGrade());
     }
 }
