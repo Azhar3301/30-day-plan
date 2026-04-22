46b075134d46e7c7417bb04c14ff68c315f080a6
Azhar3301 <it24031@mbstu.ac.bd>
2026-04-22 09:26:59 +0600

    Update Example2.java

--- Day 3/Example2.java
+++ Day 3/Example2.java
@@ -2,9 +2,8 @@ class Car {
     String brand;
     String color;
     int year;
-
     Car() {
-        this("Toyota", "White", 2020);
+        this("BMW", "RED",2202);
     }
 
     Car(String brand) {
@@ -12,20 +11,17 @@ class Car {
     }
 
     Car(String brand, String color, int year) {
-        this.brand = brand;
-        this.color = color;
-        this.year  = year;
+        this.brand=brand;
+        this.color=color;
+        this.year=year;
     }
-
     void display() {
-        System.out.println(brand + " | " + color + " | " + year);
+        System.out.println(brand+" | "+ color+" | "+year);
     }
-
     public static void main(String[] args) {
         Car c1 = new Car();
         Car c2 = new Car("Honda");
-        Car c3 = new Car("BMW", "Red", 2023);
-
+        Car c3 = new Car("FORD", "Pink", 2028);
         c1.display();
         c2.display();
         c3.display();
